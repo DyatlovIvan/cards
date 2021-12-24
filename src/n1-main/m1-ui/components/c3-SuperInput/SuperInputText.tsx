fa33bdb608b5dd49071b28449aa24f3000f3a3be
DyatlovIvan <diatlovivan92@mail.ru>
2021-12-25 00:58:21 +0400

    initial commit

---
+++ src/n1-main/m1-ui/components/c3-SuperInput/SuperInputText.tsx
@@ -0,0 +1,43 @@
+import {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes, KeyboardEventHandler} from "react";
+import s from './SuperInputText.module.css'
+
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+type SuperInputTextPropsType = DefaultInputPropsType & { // и + ещё пропсы которых нет в стандартном инпуте
+    onChangeText?: (value: string) => void
+    onEnter?: () => void
+    error?: string
+    spanClassName?: string
+}
+
+export const SuperInputText = ({type,onChange,onChangeText,
+                                   onKeyPress,onEnter,
+                                   error,className,
+                                   spanClassName,...props}:SuperInputTextPropsType) =>{
+
+    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+
+        onChange && onChange(e)
+
+        onChangeText && onChangeText(e.currentTarget.value)
+    }
+    const onKeyPressCallback = (e: KeyboardEventHandler<HTMLInputElement>) => {
+        // onKeyPress && onKeyPress(e);
+        //
+        // onEnter && e.key === 'Enter' && onEnter()
+    }
+    const finalSpanClassName = `${s.error} ${spanClassName ? spanClassName : ''}`
+    const finalInputClassName = `${s.input} ${error ? s.errorInput:s.superInput}`
+
+    return(
+        <>
+            <input
+                type={'text'}
+                onChange={onChangeCallback}
+                // onKeyPress={onKeyPressCallback}
+                className={finalInputClassName}
+                {...props}
+            />
+            {error && <span className={finalSpanClassName}>{error}</span>}
+        </>
+    )
+}
--- src/n2-features/f5-allComponents/ShowAllComponents.tsx
+++ src/n2-features/f5-allComponents/ShowAllComponents.tsx
@@ -1,5 +1,6 @@
 import {SuperButton} from "../../n1-main/m1-ui/components/c1-SuperButton/SuperButton";
 import {SuperCheckbox} from "../../n1-main/m1-ui/components/c2-SuperCheckbox/SuperCheckbox";
+import {SuperInputText} from "../../n1-main/m1-ui/components/c3-SuperInput/SuperInputText";
 
 export const ShowAllComponents = () => {
     return (
@@ -10,10 +11,11 @@ export const ShowAllComponents = () => {
                 <SuperButton disabled>disabled</SuperButton>
             </div>
             <div>
-                <SuperCheckbox>
-                    some text
-                </SuperCheckbox>
+                <SuperCheckbox>some text</SuperCheckbox>
                 <SuperCheckbox/>
+            </div>
+            <div>
+                <SuperInputText/>
             </div>
         </div>
     )
