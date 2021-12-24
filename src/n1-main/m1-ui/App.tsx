c58fd8720f333c38195afbd0b0a16a05f2237eb7
DyatlovIvan <diatlovivan92@mail.ru>
2021-12-24 16:43:50 +0400

    init commit

---
+++ src/n1-main/m1-ui/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      //hr//prov
+      <>
+        {/*<Header/>*/}
+        {/*<Main/>*/}
+      </>
+    </div>
+  );
+}
+
+export default App;
