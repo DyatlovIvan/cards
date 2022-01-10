dd3c67846f05ca9c4bd30276ca2fd254f58319ae
DyatlovIvan <diatlovivan92@mail.ru>
2022-01-11 00:24:17 +0400

    Api instance

--- src/n1-main/m3-dal/api.ts
+++ src/n1-main/m3-dal/api.ts
@@ -1,3 +1,20 @@
+import axios from "axios"
+
+const settings = {
+    withCredentials: true,
+}
+
+// const instance = axios.create({
+//     baseURL: 'https://neko-back.herokuapp.com/2.0',
+//     ...settings
+// })
+
+const instance = axios.create({
+    baseURL: 'http://localhost:7542/2.0/',
+    ...settings
+})
+
+
 export const authAPI = {
 
 }
