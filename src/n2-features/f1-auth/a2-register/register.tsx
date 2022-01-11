508c6699904e846bba8baf374829f13c1f0ef139
ÐEvgeniy Danchenko <evgeniydanchenko92@gmail.com>
2022-01-11 13:38:34 +0300

    init register

--- src/n2-features/f1-auth/a2-register/register.tsx
+++ src/n2-features/f1-auth/a2-register/register.tsx
@@ -1,7 +1,19 @@
+import {SuperInputText} from "../../../n1-main/m1-ui/components/c3-SuperInput/SuperInputText";
+import {SuperButton} from "../../../n1-main/m1-ui/components/c1-SuperButton/SuperButton";
+
 export const Register = () =>{
     return(
-        <div>
-            Register
-        </div>
+        <>
+            <h1>It Incubator</h1>
+            <h2>Sign in</h2>
+            <form>
+                <SuperInputText/>
+                <SuperInputText/>
+                <SuperInputText/>
+                <SuperButton/>
+                <SuperButton/>
+            </form>
+
+        </>
     )
 }
