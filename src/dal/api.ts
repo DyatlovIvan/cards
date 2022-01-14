09e458b7d874d906d254379289ec998946a3843b
DyatlovIvan <diatlovivan92@mail.ru>
2022-01-14 19:06:14 +0400

    heroku

--- src/dal/api.ts
+++ src/dal/api.ts
@@ -10,15 +10,15 @@ const settings = {
     withCredentials: true,
 }
 
-// const instance = axios.create({
-//     baseURL: 'https://neko-back.herokuapp.com/2.0',
-//     ...settings
-// })
-
 const instance = axios.create({
-    baseURL: 'http://localhost:7542/2.0/',
+    baseURL: 'https://neko-back.herokuapp.com/2.0',
     ...settings
 })
+
+// const instance = axios.create({
+//     baseURL: 'http://localhost:7542/2.0/',
+//     ...settings
+// })
 
 
 export const authAPI = {
