455f1221adbe476eed418b0439c692f241cfec06
DimaShumanskiy <dimaihfn@gmail.com>
2022-01-17 17:40:01 +0300

    style email spam

--- src/ui/password/email.tsx
+++ src/ui/password/email.tsx
@@ -6,7 +6,18 @@ export const Email = React.memo(() => {
     return (
 
         <div className={s.main}>
-
+            <div className={s.check}>
+                <div className={s.check_body}>
+                    <h2 className={s.check_title}>It-incubator</h2>
+                    <div className={s.check_image}>
+                        <img className={s.check_img} src={emailImg} alt=""/>
+                    </div>
+                    <div className={s.check_subtitle}>Check Email</div>
+                    <div className={s.check_text}>We’ve sent an Email with instructions to <br/>
+                        example@mail.com
+                    </div>
+                </div>
+            </div>
         </div>
 
     );
