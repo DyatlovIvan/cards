c65350c1de4934e4d429f8754ca38dde76a3922e
DyatlovIvan <diatlovivan92@mail.ru>
2022-01-21 19:22:34 +0400

    heroku

--- src/dal/api.ts
+++ src/dal/api.ts
@@ -3,15 +3,15 @@ import {cardPacksType} from "../bll/packsReducer";
 
 
 
-// const instance = axios.create({
-//     baseURL: 'https://neko-back.herokuapp.com/2.0',
-//      withCredentials: true
-// })
-
 const instance = axios.create({
-    baseURL: 'http://localhost:7542/2.0/',
-    withCredentials: true
+    baseURL: 'https://neko-back.herokuapp.com/2.0',
+     withCredentials: true
 })
+
+// const instance = axios.create({
+//     baseURL: 'http://localhost:7542/2.0/',
+//     withCredentials: true
+// })
 
 
 export const authAPI = {
