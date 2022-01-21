e7912d648feceed4458af65c80291e40105b3d75
DyatlovIvan <diatlovivan92@mail.ru>
2022-01-21 14:50:52 +0400

    correct

--- src/ui/packs/pack.tsx
+++ src/ui/packs/pack.tsx
@@ -18,7 +18,7 @@ type PackType = {
     deletePackHandler: (id: string) => void
     updatePackHandler: (id: string) => void
     learnHandler: (id: string) => void
-    index:number
+    index: number
 }
 export const Pack = (props: PackType) => {
 
@@ -27,13 +27,14 @@ export const Pack = (props: PackType) => {
 
 
     return (
-        <div className={`${style.pack} && ${props.index%2===0?style.mainColor:style.secondColor}`}>
+        <div className={`${style.pack} && ${props.index % 2 === 0 ? style.mainColor : style.secondColor}`}>
             <div className={style.cellName}> {cutString(props.name)}</div>
             <div className={style.cardsCount}> {props.cards}</div>
             <span className={style.lastUpdated}> {lastUpdated}</span>
             <span className={style.createdBy}> {cutString(props.createdBy)}</span>
             <div className={style.buttons}>
                 {userId === props.packUserId &&
+
                 <Button className={style.button}
                         type="primary"
                         disabled={props.disabled}
@@ -46,9 +47,9 @@ export const Pack = (props: PackType) => {
                         onClick={() => props.updatePackHandler(props.id)}>Edit</Button>}
 
                 <Button className={style.button}
-                        type="primary"
                         disabled={props.disabled}
-                        onClick={() => props.learnHandler(props.id)}>Learn</Button>
+                        onClick={() => props.learnHandler(props.id)}
+                        value={'Learn'}>Learn</Button>
             </div>
         </div>
     )
